28251031c7006b75cbb3a6d1f0fba7cc1e22bd9a
Joonas Hiltunen <joonas.s.jh@gmail.com>
2022-03-28 20:03:02 +0300

    Initial Commit

---
+++ testingSwift/testingSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingSwiftApp.swift
+//  testingSwift
+//
+//  Created by iosdev on 28.3.2022.
+//
+
+import SwiftUI
+
+@main
+struct testingSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
